655f78ea4055884c546b31f5058b3c16a58e1ff3
JRDNVI <20096529.wit@gmail.com>
2023-11-17 16:40:40 +0000

    Added filter person conponent

--- movies/src/components/filterMoviesCard/index.js
+++ movies/src/components/filterMoviesCard/index.js
@@ -31,6 +31,7 @@ export default function FilterMoviesCard(props) {
     return <h1>{error.message}</h1>;
   }
   const genres = data.genres;
+  
   if (genres[0].name !== "All") {
     genres.unshift({ id: "0", name: "All" });
   }
--- movies/src/pages/popularActorPage.js
+++ movies/src/pages/popularActorPage.js
@@ -4,9 +4,11 @@ import { getPopularActors } from "../api/tmdb-api";
 import Spinner from '../components/spinner';
 import PopularActor from "../components/popularActor";
 import { Pagination } from "@mui/material";
+import FilterPersonCard from "../components/filterPersonCard";
 
 const PopularActorPage = () => {
     const [currentPage, setCurrentPage] = useState(1);
+    const [searchValue, setSearchValue] = useState("");
 
     const {data, isLoading, error, isError, refetch} = useQuery(
         ["Popular Actor", {page: currentPage}],
@@ -19,15 +21,30 @@ const PopularActorPage = () => {
       if (isError) {
         return <h1>{error.message}</h1>
       }
+      
+      let displayedPeople = data.results
+        .filter((person) => {
+          return person.name.toLowerCase().search(searchValue.toLowerCase()) !== -1;
+        })
 
       const handleChange = (event, page) => {
         setCurrentPage(page)
         refetch({ page: currentPage})
       } 
 
+      const handleFilterChange = (type, value) => {
+        if (type === "name") setSearchValue(value)
+      }
+
+    console.log(data.results)
+
     return (
         <>
-            <PopularActor data={data.results} />
+            <FilterPersonCard 
+              searchValue={searchValue}
+              onUserInput={handleFilterChange}
+            />
+            <PopularActor data={displayedPeople} />
             <Pagination count={10} color="primary" onChange={handleChange} page={currentPage} />
         </>
     );
